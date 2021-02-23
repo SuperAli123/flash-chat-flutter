366f492b9347943fd0613f03e55b1bbe8f6642c8
Ali Akbar <aliakber654@gmail.com>
2021-02-23 20:39:27 +0500

    Fork Push

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,15 +1,63 @@
 import 'package:flutter/material.dart';
 
+import 'login_screen.dart';
+import 'registration_screen.dart';
+
 class WelcomeScreen extends StatefulWidget {
+  static String id = 'welcome_screen';
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation animation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      duration: Duration(seconds: 5),
+      vsync: this,
+      // upperBound: 100.0, // can't be used when animation uses curve
+    );
+
+    animation = ColorTween(begin: Colors.white, end: Colors.blue)
+        .animate(animationController);
+
+    // animation = CurvedAnimation(
+    //   parent: animationController,
+    //   curve: Curves.decelerate,
+    //   //curve: Curves.easeIn,
+    // );
+    //animationController.forward(); // for animation forward 0 to 1;
+    animationController.reverse(from: 1.0); // for animation reverse 1 to 0;
+
+    // animation.addStatusListener((status) {
+    //   if (status == AnimationStatus.dismissed) {
+    //     animationController.forward();
+    //   } else if (status == AnimationStatus.completed) {
+    //     animationController.reverse(from: 1.0);
+    //   }
+    // });
+
+    animationController.addListener(() {
+      setState(() {});
+      print(animation.value);
+      //print(animationController.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -18,12 +66,18 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    //height: animationController.value, // dynamically increasing the height (Animation)
+                    // height: animation.value * 100,
+                    height: 60.0,
+                  ),
                 ),
                 Text(
                   'Flash Chat',
+                  // '${animationController.value.toInt()}%',
                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
@@ -42,7 +96,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 borderRadius: BorderRadius.circular(30.0),
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to login screen.
+                    Navigator.pushNamed(context, LoginScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
@@ -60,7 +114,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 elevation: 5.0,
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to registration screen.
+                    Navigator.pushNamed(context, RegistrationScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
